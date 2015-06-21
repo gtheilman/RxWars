7909aa883846b903b6cd2e8e3c6e90609011a8ac
Gary Theilman <gtheilman@gmail.com>
2015-06-21 14:32:51 -0500

    Added link to database admin page

--- client/views/settings.js
+++ client/views/settings.js
@@ -75,5 +75,104 @@ Template.settings.events({
     "click #goDatabasePage": function () {
         Router.go('/admin');
 
+    },
+
+    "click #resetDrugsButton": function () {
+        if (confirm("Are you sure you really want to reset drug list & clear all transactions and market history?")) {
+            Meteor.call('resetMarket');
+            Meteor.call('resetDrugs');
+            sAlert.success('Drug list, transactions and market history reset', {
+                effect: 'scale', position: 'top-right',
+                timeout: '8000', onRouteClose: false, stack: true, offset: '0px'
+            });
+        }
+
+    },
+
+    "click #resetAllButton": function () {
+        if (confirm("Are you sure you really want to reset everything except the admin account?")) {
+            Meteor.call('resetMarket');
+            Meteor.call('resetDrugs');
+            Meteor.call('resetPlayers');
+            sAlert.success('Players, drug list, transactions and market history reset', {
+                effect: 'scale', position: 'top-right',
+                timeout: '8000', onRouteClose: false, stack: true, offset: '0px'
+            });
+        }
+
+    },
+
+    "click #decriminalize": function () {
+        if (confirm("Are you sure you really want to set demand for all drugs to near zero?")) {
+            Meteor.call('decriminalize');
+            sAlert.success('The illicit drug market is about to bottom out...', {
+                effect: 'scale', position: 'top-right',
+                timeout: '8000', onRouteClose: false, stack: true, offset: '0px'
+            });
+        }
+    },
+
+    "click #increaseScheduleIIBuyRisk": function () {
+
+        Meteor.call('increaseScheduleIIBuyRisk', function (error, result) {
+            if (result) {
+                sAlert.success('Risk of being arrested when buying Schedule II drugs has been increased to ' + result + "%.", {
+                    effect: 'scale', position: 'top-right',
+                    timeout: '8000', onRouteClose: false, stack: true, offset: '0px'
+                });
+            } else {
+                console.log("Error: " + error);
+            }
+        });
+
+
+    },
+
+    "click #decreaseScheduleIIBuyRisk": function () {
+
+        Meteor.call('decreaseScheduleIIBuyRisk', function (error, result) {
+            if (result) {
+                sAlert.success('Risk of being arrested when buying Schedule II drugs has been decreased to ' + result + "%.", {
+                    effect: 'scale', position: 'top-right',
+                    timeout: '8000', onRouteClose: false, stack: true, offset: '0px'
+                });
+            } else {
+                console.log("Error: " + error);
+            }
+        });
+
+
+    },
+
+    "click #increaseScheduleIII_VBuyRisk": function () {
+
+        Meteor.call('increaseScheduleIII_VBuyRisk', function (error, result) {
+            if (result) {
+                sAlert.success('Risk of being arrested when buying Schedule III - V drugs has been increased to ' + result + "%.", {
+                    effect: 'scale', position: 'top-right',
+                    timeout: '8000', onRouteClose: false, stack: true, offset: '0px'
+                });
+            } else {
+                console.log("Error: " + error);
+            }
+        });
+
+
+    },
+
+    "click #decreaseScheduleIII_VBuyRisk": function () {
+
+        Meteor.call('decreaseScheduleIII_VBuyRisk', function (error, result) {
+            if (result) {
+                sAlert.success('Risk of being arrested when buying Schedule III - V drugs has been decreased to ' + result + "%.", {
+                    effect: 'scale', position: 'top-right',
+                    timeout: '8000', onRouteClose: false, stack: true, offset: '0px'
+                });
+            } else {
+                console.log("Error: " + error);
+            }
+        });
+
+
     }
 });
--- server/server.js
+++ server/server.js
@@ -8,11 +8,11 @@ if (Meteor.isServer) Meteor.methods({
             var numberAvailable = 1;
             var numberPlayers = 0;
             var price = 0;
-
+            console.log("Drug:  " + drug);
             // https://github.com/mizzao/meteor-user-status
             Meteor.users.find({"status.online": true}).forEach(function (player) {
                 if (player.username != 'admin') {
-                    console.log(player._id);
+
                     console.log(player.username);
                     numberPlayers = numberPlayers + 1;
 
@@ -29,6 +29,10 @@ if (Meteor.isServer) Meteor.methods({
 
             price = parseFloat(drug.awp) * parseFloat(drug.demandMultiplier) * 150 * numberPlayers / numberAvailable;
 
+            if (price < (1.1 * parseFloat(drug.awp))) {
+                price = 1.1 * parseFloat(drug.awp);
+            }
+
             entry = {
                 time: new Date(),
                 drug_id: drug._id,
@@ -40,11 +44,180 @@ if (Meteor.isServer) Meteor.methods({
 
         });
     },
+    'increaseScheduleIIBuyRisk': function () {
+        var buyRisk = Drugs.findOne({schedule: 'II'}).buyRisk;
+        Drugs.update({schedule: 'II'},
+            {
+                $set: {
+                    buyRisk: buyRisk + 5
+                }
+            },
+            {multi: true});
+
+        return buyRisk + 5;
+    },
+
+    'decreaseScheduleIIBuyRisk': function () {
+        var buyRisk = Drugs.findOne({schedule: 'II'}).buyRisk;
+        Drugs.update({schedule: 'II'},
+            {
+                $set: {
+                    buyRisk: buyRisk - 5
+                }
+            },
+            {multi: true});
+
+        return buyRisk - 5;
+    },
+    'increaseScheduleIII_VBuyRisk': function () {
+        var buyRisk = Drugs.findOne({schedule: 'III'}).buyRisk;
+        if (!buyRisk) {
+            var buyRisk = Drugs.findOne({schedule: 'IV'}).buyRisk;
+        }
+        if (!buyRisk) {
+            var buyRisk = Drugs.findOne({schedule: 'V'}).buyRisk;
+        }
+
+        Drugs.update({schedule: 'III'},
+            {
+                $set: {
+                    buyRisk: buyRisk + 5
+                }
+            },
+            {multi: true});
+        Drugs.update({schedule: 'IV'},
+            {
+                $set: {
+                    buyRisk: buyRisk + 5
+                }
+            },
+            {multi: true});
+        Drugs.update({schedule: 'V'},
+            {
+                $set: {
+                    buyRisk: buyRisk + 5
+                }
+            },
+            {multi: true});
+
+        return buyRisk + 5;
+    },
+
+    'decreaseScheduleIII_VBuyRisk': function () {
+        var buyRisk = Drugs.findOne({schedule: 'III'}).buyRisk;
+        if (!buyRisk) {
+            var buyRisk = Drugs.findOne({schedule: 'IV'}).buyRisk;
+        }
+        if (!buyRisk) {
+            var buyRisk = Drugs.findOne({schedule: 'V'}).buyRisk;
+        }
+
+        Drugs.update({schedule: 'III'},
+            {
+                $set: {
+                    buyRisk: buyRisk - 5
+                }
+            },
+            {multi: true});
+        Drugs.update({schedule: 'IV'},
+            {
+                $set: {
+                    buyRisk: buyRisk - 5
+                }
+            },
+            {multi: true});
+        Drugs.update({schedule: 'V'},
+            {
+                $set: {
+                    buyRisk: buyRisk - 5
+                }
+            },
+            {multi: true});
+
+        return buyRisk - 5;
+    },
+
+    'decriminalize': function () {
+        Drugs.update({},
+            {
+                $set: {
+                    demandMultiplier: 0.0001
+                }
+            }, {multi: true})
+    },
 
     'resetMarket': function () {
         Transactions.remove({});
         DrugPrice.remove({});
     },
+
+    'resetDrugs': function () {
+
+        Drugs.remove({});
+
+        Drugs.insert({
+            name: 'alprazolam',
+            schedule: 'IV',
+            awp: 2,
+            buyRisk: 10,
+            sellRisk: 20,
+            demandMultiplier: 1,
+            active: true
+        });
+
+        Drugs.insert({
+            name: 'amphetamine',
+            schedule: 'II',
+            awp: 2,
+            buyRisk: 25,
+            sellRisk: 20,
+            demandMultiplier: 1,
+            active: true
+        });
+        Drugs.insert({
+            name: 'hydrocodone',
+            schedule: 'II',
+            awp: 8.5,
+            buyRisk: 25,
+            sellRisk: 20,
+            demandMultiplier: 2,
+            active: true
+        });
+
+        Drugs.insert({
+            name: 'oxandrolone',
+            schedule: 'III',
+            awp: 1,
+            buyRisk: 10,
+            sellRisk: 20,
+            demandMultiplier: 0.5,
+            active: true
+        });
+        Drugs.insert({
+            name: 'oxycodone',
+            schedule: 'II',
+            awp: 8.5,
+            buyRisk: 25,
+            sellRisk: 20,
+            demandMultiplier: 3,
+            active: true
+        });
+
+        Drugs.insert({
+            name: 'trazodone',
+            schedule: 'N/A',
+            awp: 1,
+            buyRisk: 5,
+            sellRisk: 5,
+            demandMultiplier: 0.5,
+            active: true
+        });
+
+
+    },
+    'resetPlayers': function () {
+        Meteor.users.remove({username: {$not: /^admin.*/}});
+    }
 
 
 });
