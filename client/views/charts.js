94117285c3554fc1cbfd90fa497ff49c00ff8501
Gary Theilman <gtheilman@gmail.com>
2015-06-26 14:50:48 -0500

    Trying to get drugPriceTrends to work

--- client/views/charts.js
+++ client/views/charts.js
@@ -1,77 +1,38 @@
 Template.layout.drugPriceTrends = function () {
 
-    drugPriceTrendsFunction = function () {
-        var drugPriceTrends = "[";
+    function drugPriceTrends() {
+        var data = [];
 
         Drugs.find({active: true}, {sort: {name: 1}}).forEach(function (drug) {
-
-            drugPriceTrends += ' { name: \"';
-
-            drugPriceTrends += drug.name;
-
-            drugPriceTrends += '\", data: [';
-
+            var element = {};
+            element.name = drug.name;
+            element.data = [];
+            var time = 0;
+            var price = 0;
 
             DrugPrice.find({drug_id: drug._id}, {sort: {name: 1}}).forEach(function (pricePoint) {
                 if (pricePoint.time != '' && pricePoint.price != '') {
-                    drugPriceTrends = drugPriceTrends + "[Date.UTC(1970, 1, 1, " + moment(pricePoint.time).format('H, m, s') + "), " + pricePoint.price + "],";
+                    time = parseInt(moment(pricePoint.time).format('X'));
+                    price = pricePoint.price;
+                    element.data.push([time, price]);
                 }
-
             });
-            drugPriceTrends += "]},";
-
-
+            data.push(element);
         });
 
-        drugPriceTrends += "]";
+        console.log(data);
+        return data
 
-
-        return drugPriceTrends
-    };
-
-
-    var drugPriceTrends = drugPriceTrendsFunction();
-
-    drugPriceTrends = [
-
-        {
-            name: "alprazolam",
-            data: [
-                [Date.UTC(1970, 1, 1, 10, 18, 30), 80], [Date.UTC(1970, 1, 1, 10, 18, 46), 80], [Date.UTC(1970, 1, 1, 10, 19, 2), 80], [Date.UTC(1970, 1, 1, 10, 19, 17), 80],
-                [Date.UTC(1970, 1, 1, 10, 40, 41), 80], [Date.UTC(1970, 1, 1, 10, 40, 57), 80], [Date.UTC(1970, 1, 1, 10, 41, 12), 80],
-                [Date.UTC(1970, 1, 1, 10, 41, 29), 80],
-                [Date.UTC(1970, 1, 1, 10, 41, 44), 80],
-            ]
-        },
-
-
-        {
-            name: "amphetamine",
-            data: [[Date.UTC(1970, 1, 1, 10, 18, 30), 80], [Date.UTC(1970, 1, 1, 10, 18, 46), 80], [Date.UTC(1970, 1, 1, 10, 19, 2), 80], [Date.UTC(1970, 1, 1, 10, 19, 17), 80], [Date.UTC(1970, 1, 1, 10, 40, 41), 80], [Date.UTC(1970, 1, 1, 10, 40, 57), 80], [Date.UTC(1970, 1, 1, 10, 41, 12), 80], [Date.UTC(1970, 1, 1, 10, 41, 29), 80], [Date.UTC(1970, 1, 1, 10, 41, 44), 80],]
-        }, {
-            name: "hydrocodone",
-            data: [[Date.UTC(1970, 1, 1, 10, 18, 30), 340], [Date.UTC(1970, 1, 1, 10, 18, 46), 340], [Date.UTC(1970, 1, 1, 10, 19, 2), 340], [Date.UTC(1970, 1, 1, 10, 19, 17), 340], [Date.UTC(1970, 1, 1, 10, 40, 41), 340], [Date.UTC(1970, 1, 1, 10, 40, 57), 340], [Date.UTC(1970, 1, 1, 10, 41, 12), 340], [Date.UTC(1970, 1, 1, 10, 41, 29), 340], [Date.UTC(1970, 1, 1, 10, 41, 44), 340],]
-        }, {
-            name: "oxandrolone",
-            data: [[Date.UTC(1970, 1, 1, 10, 18, 30), 40], [Date.UTC(1970, 1, 1, 10, 18, 46), 40], [Date.UTC(1970, 1, 1, 10, 19, 2), 40], [Date.UTC(1970, 1, 1, 10, 19, 17), 40], [Date.UTC(1970, 1, 1, 10, 40, 41), 40], [Date.UTC(1970, 1, 1, 10, 40, 57), 40], [Date.UTC(1970, 1, 1, 10, 41, 12), 40], [Date.UTC(1970, 1, 1, 10, 41, 29), 40], [Date.UTC(1970, 1, 1, 10, 41, 44), 40],]
-        }, {
-            name: "oxycodone",
-            data: [[Date.UTC(1970, 1, 1, 10, 18, 30), 340], [Date.UTC(1970, 1, 1, 10, 18, 46), 340], [Date.UTC(1970, 1, 1, 10, 19, 2), 340], [Date.UTC(1970, 1, 1, 10, 19, 17), 340], [Date.UTC(1970, 1, 1, 10, 40, 41), 340], [Date.UTC(1970, 1, 1, 10, 40, 57), 340], [Date.UTC(1970, 1, 1, 10, 41, 12), 340], [Date.UTC(1970, 1, 1, 10, 41, 29), 340], [Date.UTC(1970, 1, 1, 10, 41, 44), 340],]
-        }, {
-            name: "trazodone",
-            data: [[Date.UTC(1970, 1, 1, 10, 18, 30), 40], [Date.UTC(1970, 1, 1, 10, 18, 46), 40], [Date.UTC(1970, 1, 1, 10, 19, 2), 40], [Date.UTC(1970, 1, 1, 10, 19, 17), 40], [Date.UTC(1970, 1, 1, 10, 40, 41), 40], [Date.UTC(1970, 1, 1, 10, 40, 57), 40], [Date.UTC(1970, 1, 1, 10, 41, 12), 40], [Date.UTC(1970, 1, 1, 10, 41, 29), 40], [Date.UTC(1970, 1, 1, 10, 41, 44), 40],]
-        },]
-
-    console.log(drugPriceTrends);
+    }
 
 
     return {
         chart: {
             type: 'spline'
         },
-        title: {
-            text: 'Prescription Drug Prices'
-        },
+            title: {
+                text: 'Prescription Drug Prices'
+            },
         subtitle: {
             text: 'Average Street Prices'
         },
@@ -96,9 +57,12 @@ Template.layout.drugPriceTrends = function () {
         },
 
 
-        series: drugPriceTrends
+        series: drugPriceTrends()
 
     }
+
+
+
 
 
 }
